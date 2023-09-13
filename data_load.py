08490bd9839034f361abbdea84fc1b2904697ede
Michael Dereszynski <mlderes@hotmail.com>
2023-09-13 11:02:38 -0500

    initial commit

---
+++ data_load.py
@@ -0,0 +1,21 @@
+import sqlite3
+from pathlib import Path
+
+# Define the path to the data
+# Set the database filename
+# Set the data filename
+
+# Connect to the database
+# Create a cursor object
+# Drop the table if it exists
+# Create the table
+
+# Load data from csv file
+# Skip the header row
+# Insert the data into the table
+# Commit the changes
+
+# Select all rows from the table
+# Print the result
+# Close the cursor
+# Close the connection
